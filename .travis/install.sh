b3b9a7b1e43dc649be51abb44317efc6e16f9405
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-09 12:30:30 -0200

    Testing go env at mac

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	brew unlink go
     brew install go
     brew install glide
+    go env
 else
 	# Install glide
     bash <(curl -s https://gist.githubusercontent.com/franciscocpg/ab10b57898978009638f/raw/)
