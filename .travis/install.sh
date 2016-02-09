8d03a4b64a975a6ead7952574e86c6440272acfd
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-09 12:38:13 -0200

    Trying to undestand mac os env

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	brew unlink go
+	brew upgrade
     brew install go
     brew install glide
     go env
+    echo $(pwd)
 else
 	# Install glide
     bash <(curl -s https://gist.githubusercontent.com/franciscocpg/ab10b57898978009638f/raw/)
