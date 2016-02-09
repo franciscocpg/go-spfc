63030939ad3008c19f4ee549912283b5cd787567
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-09 13:00:52 -0200

    Testing travis and mac build

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	export GOPATH="$HOME/go"
 	brew unlink go
 	brew update
     brew install go
+    mkdir -p "$GO_PATH/src/github.com"
+    cp -R ../../franciscocpg "$GOPATH"
+    cd "$GOPATH/src/github.com/franciscocpg/go-spfc"
     brew install glide
     go env
     echo $(pwd)
