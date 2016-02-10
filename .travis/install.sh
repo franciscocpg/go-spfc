1ffa93594fbcb04c30637098fc31fef28be6cad4
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-09 22:16:57 -0200

    Missing go bin path for mac

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,7 @@ set -e
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	. ./.travis/mac-env
 	brew unlink go
+    echo "Updating brew..."
 	brew update > /dev/null
     brew install go
     mkdir -p "$GITHUB"
