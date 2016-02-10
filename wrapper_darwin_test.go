fb0f505f43a94ae9aa9f227ccb8945a697e2ef3b
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-09 22:32:38 -0200

    go lint free

--- wrapper_darwin_test.go
+++ wrapper_darwin_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const go_spfc_test_plist string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
+const goSpfcTestPlist string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
 	"<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"> " +
 	"<plist version=\"1.0\"> " +
 	"<dict> " +
