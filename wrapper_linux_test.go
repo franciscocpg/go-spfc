0622885a26bdf221c49af7ff3df2fa4d8f9deb4f
Francisco Guimarães <francisco.cpg@gmail.com>
2016-02-12 06:35:05 -0200

    Implementing systemd service go-spfc-test

--- wrapper_linux_test.go
+++ wrapper_linux_test.go
@@ -4,6 +4,16 @@ import "fmt"
 
 var fileName = fmt.Sprintf("/etc/init/%s.conf", servNameTest)
 
+const goSpfcTestSystemd string = "[Unit] " +
+	"Description=go-spfc-test " +
+	" " +
+	"[Service] " +
+	"TimeoutStartSec=0 " +
+	"ExecStart=/bin/sh -c 'while true; do echo Hello World; sleep 1; done' " +
+	" " +
+	"[Install] " +
+	"WantedBy=multi-user.target "
+
 func createService() {
 	fmt.Println(fileName)
 	cmd := fmt.Sprintf("echo \"script\n  while : ; do sleep 1 ; done\nend script\" > %s", fileName)
