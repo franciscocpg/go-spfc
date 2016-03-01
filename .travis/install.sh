3989120cb717324f01e0535b3e45ce15a871dacf
Francisco Guimarães <francisco.cpg@gmail.com>
2016-03-01 16:21:27 -0300

    Lets try travis then

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,8 +12,13 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install glide
     echo $(pwd)
 else
-	# Install glide
-    bash <(curl -s https://gist.githubusercontent.com/franciscocpg/ab10b57898978009638f/raw/)
+    if [[ "$DOCKER" = "true" ]]; then
+        docker run -itd --name systemd -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /tmp/$(mktemp -d):/run milcom/centos7-systemd
+        docker ps -a
+    else
+        # Install glide
+        bash <(curl -s https://gist.githubusercontent.com/franciscocpg/ab10b57898978009638f/raw/)
+    fi
 fi
 go get -u github.com/golang/lint/golint
 glide up
