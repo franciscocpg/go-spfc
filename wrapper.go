bbcb78f1b51d20fc2a32ab9e6a961e07f7289a44
Francisco Guimarães <francisco.cpg@gmail.com>
2016-03-15 20:21:17 -0300

    Added wait for start and stop

--- wrapper.go
+++ wrapper.go
@@ -4,6 +4,7 @@ package service
 import (
 	"errors"
 	"os/exec"
+	"time"
 )
 
 var (
@@ -49,26 +50,44 @@ func init() {
 	controlType, srvControl = getControlType()
 }
 
-// NewExecution constructs a execution with a given namh.
+// NewHandler constructs a handler with a given name.
 // In linux with sudo true and Mac sudo false
 func NewHandler(serviceName string) *Handler {
 	return &Handler{sudoDefault(), serviceName}
 }
 
-// Start starts service
+// Start starts a service
 func (h *Handler) Start() (Status, error) {
 	return h.execService(srvControl.startCmd(h.ServiceName))
 }
 
-// GetStatus show the status for a given service name
+// Start starts a service and wait it starts
+func (h *Handler) StartAndWait(timeout time.Duration) (Status, error) {
+	_, err := h.Start()
+	if err != nil {
+		return Status{}, err
+	}
+	return h.waitTimeout(false, timeout)
+}
+
+// GetStatus show the status for a service
 func (h *Handler) GetStatus() (Status, error) {
 	out, err := h.execServiceCmd(srvControl.statusCmd(h.ServiceName))
 	return srvControl.parseStatus(out, err)
 }
 
-// Stop stops service
+// Stop stops a service
 func (h *Handler) Stop() (Status, error) {
 	return h.execService(srvControl.stopCmd(h.ServiceName))
+}
+
+// Stop stops a service and wait it stops
+func (h *Handler) StopAndWait(timeout time.Duration) (Status, error) {
+	_, err := h.Stop()
+	if err != nil {
+		return Status{}, err
+	}
+	return h.waitTimeout(true, timeout)
 }
 
 func (h *Handler) execService(cmdArr []string) (Status, error) {
@@ -84,6 +103,33 @@ func (h *Handler) execServiceCmd(cmdArr []string) (string, error) {
 		return execCmd("sudo", cmdArr...)
 	}
 	return execCmd(cmdArr[0], cmdArr[1:len(cmdArr)]...)
+}
+
+func (h *Handler) waitTimeout(running bool, timeout time.Duration) (Status, error) {
+	timeoutChannel := make(chan Status, 1)
+	go func() {
+		st, _ := h.GetStatus()
+		for getRunningCondition(running, st) {
+			st, _ = h.GetStatus()
+			time.Sleep(100 * time.Millisecond)
+		}
+		timeoutChannel <- st
+	}()
+
+	select {
+	case res := <-timeoutChannel:
+		return res, nil
+	case <-time.After(timeout):
+		return Status{}, errors.New("timeout after " + timeout.String())
+	}
+}
+
+func getRunningCondition(running bool, st Status) bool {
+	if running {
+		return st.Running
+	} else {
+		return !st.Running
+	}
 }
 
 func execCmd(cmd string, arg ...string) (string, error) {
