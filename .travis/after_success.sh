1293e6ea4bbdd1a38bc85514bacd03fc4b7f7a5b
Francisco Guimarães <francisco.cpg@gmail.com>
2016-04-04 05:17:54 -0300

    Branch for docker

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -7,6 +7,8 @@ fi
 if [[ "$DOCKER" = "true" ]]; then
 	PROJECT="/root/go/src/github.com/franciscocpg/go-spfc"
 	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	pwd
+	echo "BRANCH: $BRANCH"
 	docker exec go-systemd-test bash -l -c 'cd '$PROJECT'; bash <(curl -s https://codecov.io/bash) -f $(cat coverage-filename) -t '$CODECOV_TOKEN' -b '$BRANCH''
 else
 	bash <(curl -s https://codecov.io/bash) -f $(cat coverage-filename)
